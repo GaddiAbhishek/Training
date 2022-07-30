fdd245b7e9c898e2229ad52a015181986706b2bf
GaddiAbhishek <88286486+GaddiAbhishek@users.noreply.github.com>
2022-07-30 17:44:58 +0530

    via upload

---
+++ Fresher-Training/src/main/java/com/sanketika/product/Fresher_Training/App.java
@@ -0,0 +1,13 @@
+package com.sanketika.product.Fresher_Training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
